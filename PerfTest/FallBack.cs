b65496b1d6b7959525122dc1fe44513775248db0
Anton Drachev <anton@drachev.com>
2014-10-06 01:53:01 +0400

    added some perf tests

---
+++ PerfTest/FallBack.cs
@@ -0,0 +1,60 @@
+﻿namespace PerfTest
+{
+    public static class FallBack
+    {
+        public static int PopCnt(long value)
+        {
+            return PopCnt((ulong)value);
+        }
+
+        public static int PopCnt(ulong value)
+        {
+            var count = 0;
+            for (; value != 0; count++)
+                value &= value - 1uL;
+            return count;
+        }
+
+        public static int PopCnt(int value)
+        {
+            return PopCnt((uint)value);
+        }
+
+        public static int PopCnt(uint value)
+        {
+            var count = 0;
+            for (; value != 0; count++)
+                value &= value - 1u;
+            return count;
+        }
+
+        static readonly int[] bsIndex64 = new[] {
+             0, 47,  1, 56, 48, 27,  2, 60,
+            57, 49, 41, 37, 28, 16,  3, 61,
+            54, 58, 35, 52, 50, 42, 21, 44,
+            38, 32, 29, 23, 17, 11,  4, 62,
+            46, 55, 26, 59, 40, 36, 15, 53,
+            34, 51, 20, 43, 31, 22, 10, 45,
+            25, 39, 14, 33, 19, 30,  9, 24,
+            13, 18,  8, 12,  7,  6,  5, 63
+        };
+
+        public static int BitScanForward(long value)
+        {
+            const ulong debruijn64 = 0x03F79D71B4CB0A89uL;
+            return bsIndex64[((ulong)(value ^ (value - 1)) * debruijn64) >> 58];
+        }
+
+        public static int BitScanReverse(long value)
+        {
+            const ulong debruijn64 = 0x03F79D71B4CB0A89uL;
+            value |= value >> 1;
+            value |= value >> 2;
+            value |= value >> 4;
+            value |= value >> 8;
+            value |= value >> 16;
+            value |= value >> 32;
+            return bsIndex64[((ulong)value * debruijn64) >> 58];
+        }
+    }
+}
---
+++ PerfTest/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace PerfTest
+{
+    static class Program
+    {
+        const int runs = 100000000;
+        const int testDataLen = 1 << 14;
+        const int testDataMask = testDataLen - 1;
+        static ulong xorshift128plus_s0 = 123456789;
+        static ulong xorshift128plus_s1 = ~1234567890uL;
+        static ulong[] defaultData = Enumerable.Range(0, testDataLen).Select(_ => Xorshift128plus()).ToArray();
+        static ulong[] sparseData = Enumerable.Range(0, testDataLen).Select(_ => GetSparseBits()).ToArray();
+
+        static void Main(string[] args)
+        {
+            // warmup
+            Console.WriteLine("Patch status: " + Intrinsic.Status, Intrinsic.PopCnt(1L), Intrinsic.BitScanForward(1L));
+
+            TestRun("Rng", sw => {
+                var sum = 0uL;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += defaultData[i & testDataMask];
+                return (int)sum;
+            });
+
+            TestRun("BitScanForward", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += Intrinsic.BitScanForward((long)defaultData[i & testDataMask]);
+                return sum;
+            });
+            TestRun("FallBack.BitScanForward", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += FallBack.BitScanForward((long)defaultData[i & testDataMask]);
+                return sum;
+            });
+
+            TestRun("PopCnt64", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += Intrinsic.PopCnt(defaultData[i & testDataMask]);
+                return sum;
+            });
+            TestRun("FallBack.PopCnt64", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += FallBack.PopCnt(defaultData[i & testDataMask]);
+                return sum;
+            });
+
+            TestRun("PopCnt32", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += Intrinsic.PopCnt((uint)defaultData[i & testDataMask]);
+                return sum;
+            });
+            TestRun("FallBack.PopCnt32", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += FallBack.PopCnt((uint)defaultData[i & testDataMask]);
+                return sum;
+            });
+
+            TestRun("PopCnt64 on sparse data", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += Intrinsic.PopCnt(sparseData[i & testDataMask]);
+                return sum;
+            });
+            TestRun("FallBack.PopCnt64 on sparse data", sw => {
+                var sum = 0;
+                sw.Start();
+                for (var i = 0; i < runs; i++)
+                    sum += FallBack.PopCnt(sparseData[i & testDataMask]);
+                return sum;
+            });
+
+            Console.WriteLine("done.");
+            Console.ReadKey();
+        }
+
+        static void TestRun(string message, Func<Stopwatch, int> func)
+        {
+            //reset random
+            xorshift128plus_s0 = 123456789;
+            xorshift128plus_s1 = ~1234567890uL;
+
+            Console.WriteLine("Testing " + message + "...");
+            var sw = new Stopwatch();
+            var sum = func(sw);
+            sw.Stop();
+            Console.WriteLine("{0}ms, sum={1}", sw.ElapsedMilliseconds, sum);
+            Console.WriteLine();
+        }
+
+        static ulong GetSparseBits()
+        {
+            var result = 0uL;
+            for (var i = 0; i < 8; i++) {
+                result |= 1uL << (int)(Xorshift128plus() & 63);
+            }
+            return result;
+        }
+
+        static ulong Xorshift128plus()
+        {
+            var s1 = xorshift128plus_s0;
+            var s0 = xorshift128plus_s1;
+            xorshift128plus_s0 = s0;
+            s1 ^= s1 << 23;
+            return (xorshift128plus_s1 = (s1 ^ s0 ^ (s1 >> 17) ^ (s0 >> 26))) + s0;
+        }
+    }
+}
